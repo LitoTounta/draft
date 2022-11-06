dc02e5ca1b15212c3395b7a0b85cb288cbc928e6
tount <let00047@students.stir.ac.uk>
2022-11-06 09:23:51 +0000

    Change

--- hello.java
+++ hello.java
@@ -3,5 +3,7 @@ package practical1;
 public class Practical1 {
 public static void main(String[] args) {
         System.out.println("Hello, world!");
+	System.out.println("This is a draft");
+	System.out.println("This is a 2nd draft");
     }
 }
